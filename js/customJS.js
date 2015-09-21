59de0c2564d9d0342ab62f67a05622f74785d17d
mramachandra@deloitte.com <mramachandra@deloitte.com>
2015-09-21 18:06:19 +0530

    Merge origin/gh-pages into gh-pages
    Conflicts:
    	index.html

---
+++ js/customJS.js
@@ -0,0 +1,11 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+var c = document.getElementById("cmeme");
+var ctx = c.getContext("2d");
+ctx.fillStyle = "#FF0000";
+ctx.fillRect(0,0,150,75);
